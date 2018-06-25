71edaddac457d92edd2a085c60252cbdbb815df3
Gowtham Behara <gothsiddu@gmail.com>
2018-06-25 22:24:55 +0530

    Implement linearSearch recursively

--- Search/Array/LinearSearch.c
+++ Search/Array/LinearSearch.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "../../Helper/Array/printArray.h"
 
-int indexOf(int *arr, int len, int key) {
+int linearSearchItr(int *arr, int len, int key) {
     for (int i = 0; i < len; i++) {
         if (arr[i] == key) {
             return i;
@@ -11,35 +11,50 @@ int indexOf(int *arr, int len, int key) {
     return -1;
 }
 
+int linearSearchRec(int *arr, int len, int key) {
+    if (0 == len) {
+        return -1;
+    } else if (key == arr[len - 1]) {
+        return len - 1;
+    } else {
+        return linearSearchRec(arr, len - 1, key);
+    }
+}
+
 int main() {
   int arr[] = { 10, 20, 80, 30, 60, 50, 110, 100, 130, 170 };
   int len = sizeof(arr) / sizeof(arr[0]);
-  int res, key;
+  int resItr, resRec, key;
   
   key = 5;
-  res = indexOf(arr, len, key);
+  resItr = linearSearchItr(arr, len, key);
+  resRec = linearSearchRec(arr, len, key);
   printIntArray(arr, len);
-  printf("key = %d, res = %d\n", key, res);
+  printf("key = %d, resItr = %d, resRec = %d\n", key, resItr, resRec);
 
   key = 20;
-  res = indexOf(arr, len, key);
+  resItr = linearSearchItr(arr, len, key);
+  resRec = linearSearchRec(arr, len, key);
   printIntArray(arr, len);
-  printf("key = %d, res = %d\n", key, res);
+  printf("key = %d, resItr = %d, resRec = %d\n", key, resItr, resRec);
 
   key = 110;
-  res = indexOf(arr, len, key);
+  resItr = linearSearchItr(arr, len, key);
+  resRec = linearSearchRec(arr, len, key);
   printIntArray(arr, len);
-  printf("key = %d, res = %d\n", key, res);
+  printf("key = %d, resItr = %d, resRec = %d\n", key, resItr, resRec);
 
   key = 170;
-  res = indexOf(arr, len, key);
+  resItr = linearSearchItr(arr, len, key);
+  resRec = linearSearchRec(arr, len, key);
   printIntArray(arr, len);
-  printf("key = %d, res = %d\n", key, res);
+  printf("key = %d, resItr = %d, resRec = %d\n", key, resItr, resRec);
 
   key = 180;
-  res = indexOf(arr, len, key);
+  resItr = linearSearchItr(arr, len, key);
+  resRec = linearSearchRec(arr, len, key);
   printIntArray(arr, len);
-  printf("key = %d, res = %d\n", key, res);
+  printf("key = %d, resItr = %d, resRec = %d\n", key, resItr, resRec);
 
   return 0;
 }
