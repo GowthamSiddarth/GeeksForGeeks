6e88006831081c2f2887ba8e5e16dc3d4f9bd915
Gowtham Behara <gothsiddu@gmail.com>
2018-06-25 22:12:39 +0530

    Implement Linear Search on int arr

---
+++ Search/Array/LinearSearch.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "../../Helper/printArray.h"
+
+int indexOf(int *arr, int len, int key) {
+    for (int i = 0; i < len; i++) {
+        if (arr[i] == key) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+  int arr[] = { 10, 20, 80, 30, 60, 50, 110, 100, 130, 170 };
+  int len = sizeof(arr) / sizeof(arr[0]);
+  int res, key;
+  
+  key = 5;
+  res = indexOf(arr, len, key);
+  printIntArray(arr, len);
+  printf("key = %d, res = %d\n", key, res);
+
+  key = 20;
+  res = indexOf(arr, len, key);
+  printIntArray(arr, len);
+  printf("key = %d, res = %d\n", key, res);
+
+  key = 110;
+  res = indexOf(arr, len, key);
+  printIntArray(arr, len);
+  printf("key = %d, res = %d\n", key, res);
+
+  key = 170;
+  res = indexOf(arr, len, key);
+  printIntArray(arr, len);
+  printf("key = %d, res = %d\n", key, res);
+
+  key = 180;
+  res = indexOf(arr, len, key);
+  printIntArray(arr, len);
+  printf("key = %d, res = %d\n", key, res);
+
+  return 0;
+}
