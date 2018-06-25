ab54f5acd42ad8e9a21e0efb76ea48bb31e69034
Gowtham Behara <gothsiddu@gmail.com>
2018-06-25 22:13:00 +0530

    Format code in printArray.h

--- Helper/printArray.h
+++ Helper/printArray.h
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
-void printIntArray(int *arr, int len)
-{
-    if (len > 0)
-    {
-        for (int i = 0; i < len; i++)
-        {
-            printf("%d ", arr[i]);
-        }
-        printf("\n");
+void printIntArray(int *arr, int len) {
+  if (len > 0) {
+    for (int i = 0; i < len; i++) {
+      printf("%d ", arr[i]);
     }
+    printf("\n");
+  }
 }
