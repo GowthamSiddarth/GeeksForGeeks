ec65af318159f5e38b670c92393c01bedf5d6b7b
Gowtham Behara <gothsiddu@gmail.com>
2018-06-28 23:14:58 +0530

    Implement selection sort

---
+++ Helper/Array/Sort/selectionSort.h
@@ -0,0 +1,16 @@
+#include "../Util/swap.h"
+
+void selectionSort(int *arr, int len) {
+    for (int i = 0; i < len - 1; i++) {
+        int currMin = arr[i], currMinIdx = i;
+        for (int j = i + 1; j < len; j++) {
+            if (arr[j] < currMin) {
+                currMin = arr[j];
+                currMinIdx = j;
+            }
+        }
+        if (currMinIdx != i) {
+            swapInt(&arr[currMinIdx], &arr[i]);
+        }        
+    }
+}
