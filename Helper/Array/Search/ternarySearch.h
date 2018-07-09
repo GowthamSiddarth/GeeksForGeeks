93b91f592f50408bf7f3e53a972092d0393412ca
Gowtham Behara <gothsiddu@gmail.com>
2018-07-09 09:06:21 +0530

    Implement ternarySearch iteratively

--- Helper/Array/Search/ternarySearch.h
+++ Helper/Array/Search/ternarySearch.h
@@ -1,3 +1,29 @@
+int ternarySearchItr(int *arr, int len, int key) {
+    int low = 0, high = len - 1;
+
+    while (low <= high) {
+        int mid1 = low + (high - low) / 3;
+        int mid2 = mid1 + (high - low) / 3;
+
+        if (key < arr[mid1]) {
+            high = mid1 - 1;
+        } else if (key > arr[mid1] && key < arr[mid2]) {
+            low = mid1 + 1;
+            high = mid2 - 1;
+        } else if (key > arr[mid2]) {
+            low = mid2 + 1;
+        } else if (key == arr[mid1]) {
+            return mid1;
+        } else if (key == arr[mid2]) {
+            return mid2;
+        } else {
+            return -1;
+        }
+    }
+
+    return -1;
+}
+
 int ternarySearchHelper(int *arr, int low, int high, int key) {
     if (low <= high) {
         int mid1 = low + (high - low) / 3;
