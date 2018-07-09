cd17af1d27bd8555c00d3d9437ca1e376b1c6082
Gowtham Behara <gothsiddu@gmail.com>
2018-07-09 09:02:26 +0530

    Use ternarySearchRec to find key in sorted int arr

---
+++ DS/Array/Search/TernarySearch.c
@@ -0,0 +1,21 @@
+#include "../../../Helper/Array/Search/ternarySearch.h"
+#include <stdio.h>
+#include "../../../Helper/Array/Util/printArray.h"
+
+int main () {
+    int arr[] = {2, 3, 4, 10, 40};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    printIntArray(arr, len);
+
+    int queries[] = {1, 2, 4, 5, 11, 10, 40, 45, 50};
+    int numOfQueries = sizeof(queries) / sizeof(queries[0]);
+    for (int i = 0; i < numOfQueries; i++) {
+        int key = queries[i];
+        //int resItr = ternarySearchRec(arr, len, key);
+        int resRec = ternarySearchRec(arr, len, key);
+
+        printf("key = %d, resRec = %d\n", key, resRec);
+    }
+
+    return 0;
+}
