21f989aca404f5fce29fab186f5fb1d523262a65
Gowtham Behara <gothsiddu@gmail.com>
2018-07-19 21:42:23 +0530

    Use fibonacci func to calc nth fibonacci

--- Math/NthFibonacci.c
+++ Math/NthFibonacci.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include "../Helper/Math/fibonacci.h"
 
 int getNthFibonacci(int n) {
-    return 0;
+    return fibonacci(n);
 }
 
 int main () {
+    int queries[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int numOfQueries = sizeof(queries) / sizeof(queries[0]);
+
+    for (int i = 0; i < numOfQueries; i++) {
+        int n = queries[i];
+        int res = getNthFibonacci(n);
+
+        printf("n = %d, res = %d\n", n, res);
+    }
+
     return 0;
 }
