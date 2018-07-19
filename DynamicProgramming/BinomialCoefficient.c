a4178c6ce13cb44f073d4c25e43efc42aafde090
Gowtham Behara <gothsiddu@gmail.com>
2018-07-19 11:55:46 +0530

    Use DP to compute binomial coefficient

--- DynamicProgramming/BinomialCoefficient.c
+++ DynamicProgramming/BinomialCoefficient.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
+#include <malloc.h>
+#include "../Helper/Math/min.h"
 
 int binomialCoefficientRec(int n, int r) {
     if (n < r) {
-        return -1;
+        return 0;
     } else if (0 == r || n == r) {
         return 1;
     } else {
         return binomialCoefficientRec(n - 1, r - 1) + binomialCoefficientRec(n - 1, r);
     }
+}
+
+int binomialCoefficientWithDP(int n, int r) {
+    int **coefficients = (int **)calloc(n + 1, sizeof(int *));
+    for (int i = 0; i < (n + 1); i++) {
+        coefficients[i] = (int *)calloc(r + 1, sizeof(int));
+    }
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= minInt(i, r); j++) {
+            if (0 == j || i == j) {
+                coefficients[i][j] = 1;
+            } else {
+                coefficients[i][j] = coefficients[i - 1][j - 1] + coefficients[i - 1][j];
+            }
+        }
+    }
+
+    return coefficients[n][r];
 }
 
 int main () {
@@ -16,9 +37,10 @@ int main () {
 
     int numOfQueries = sizeof(n) / sizeof(n[0]);
     for (int i = 0; i < numOfQueries; i++) {
-        int res = binomialCoefficientRec(n[i], r[i]);
+        int res1 = binomialCoefficientRec(n[i], r[i]);
+        int res2 = binomialCoefficientWithDP(n[i], r[i]);
 
-        printf("n = %d, r = %d, res = %d\n", n[i], r[i], res);
+        printf("n = %d, r = %d, res1 = %d, res2 = %d\n", n[i], r[i], res1, res2);
     }
 
     return 0;
