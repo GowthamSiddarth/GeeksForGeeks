56f098f97a00677805be1a550cf7999d5ad01ea8
Gowtham Behara <gothsiddu@gmail.com>
2018-07-21 00:03:54 +0530

    Represent nibbles using hexadecimal %x

--- Bitwise/SwapNibblesInByte.c
+++ Bitwise/SwapNibblesInByte.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-char swapNibbles(char n) {
+unsigned char swapNibbles(unsigned char n) {
     return ((n & 0xF0) >> 4) | ((n & 0x0F) << 4);
 }
 
 int main () {
-    char queries[] = {100, 34, 255, 67, 56, 121};
+    unsigned char queries[] = {100, 34, 255, 67, 56, 121};
     int numOfQueries = sizeof(queries) / sizeof(queries[0]);
 
     for (int i = 0; i < numOfQueries; i++) {
-        char res = swapNibbles(queries[i]);
-        printf("n = %u, res = %u\n", queries[i], res);
+        unsigned char res = swapNibbles(queries[i]);
+        printf("n = %x, res = %x\n", queries[i], res);
     }
 
     return 0;
