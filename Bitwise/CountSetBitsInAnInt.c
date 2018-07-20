770812f00b5855ce3755e04cb6dfb50b2c13f53a
Gowtham Behara <gothsiddu@gmail.com>
2018-07-20 10:05:09 +0530

    Count set bits with prev num

--- Bitwise/CountSetBitsInAnInt.c
+++ Bitwise/CountSetBitsInAnInt.c
@@ -11,13 +11,25 @@ int countSetBitsWithItr(int n) {
     return count;
 }
 
+int countSetBitsWithPrevNum(int n) {
+    int count = 0;
+
+    while (n > 0) {
+        n = n & (n - 1);
+        count++;
+    }
+
+    return count;
+}
+
 int main () {
     int queries[] = {2, 4, 5, 12, 57, 98};
     int numOfQueries = sizeof(queries) / sizeof(queries[0]);
 
     for (int i = 0; i < numOfQueries; i++) {
         int res1 = countSetBitsWithItr(queries[i]);
-        printf("num = %d, res1 = %d\n", queries[i], res1);
+        int res2 = countSetBitsWithPrevNum(queries[i]);
+        printf("num = %d, res1 = %d, res2 = %d\n", queries[i], res1, res2);
     }
 
     return 0;
