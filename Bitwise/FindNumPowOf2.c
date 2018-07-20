da8cd5c9098a0d9a01ac54c7b88df837c1d07106
Gowtham Behara <gothsiddu@gmail.com>
2018-07-20 23:47:54 +0530

    Check if pow of 2 using AND op with prev num

--- Bitwise/FindNumPowOf2.c
+++ Bitwise/FindNumPowOf2.c
@@ -31,6 +31,10 @@ int isPowerOf2WithRightShift(int n) {
     return 1;
 }
 
+int isPowerOf2WithANDPrevNum(int n) {
+    return n && !(n & (n - 1));
+}
+
 int main () {
     int queries[] = {2, 3, 4, 32, 31, 45, 65, 128};
     int numOfQueries = sizeof(queries) / sizeof(queries[0]);
@@ -38,8 +42,10 @@ int main () {
     for (int i = 0; i < numOfQueries; i++) {
         int res1 = isPowerOf2WithSetBitsCount(queries[i]);
         int res2 = isPowerOf2WithLog2(queries[i]);
+        int res3 = isPowerOf2WithRightShift(queries[i]);
+        int res4 = isPowerOf2WithANDPrevNum(queries[i]);
 
-        printf("n = %d, res1 = %d, res2 = %d\n", queries[i], res1, res2);
+        printf("n = %d, res1 = %d, res2 = %d, res3 = %d, res4 = %d\n", queries[i], res1, res2, res3, res4);
     }
 
     return 0;
