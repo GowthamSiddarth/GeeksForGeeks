2e06be82d9ad0212396dc24a94a674f3d76c0da6
Gowtham Behara <gothsiddu@gmail.com>
2018-07-21 22:07:30 +0530

    Implement func to create SLL using append

---
+++ Helper/LinkedList/Util/createSLL.h
@@ -0,0 +1,14 @@
+#include "sllNode.h"
+#include "appendSLLNode.h"
+
+struct SLLNode* createIntSLL(int *arr, int len) {
+    struct SLLNode *head;
+    struct SLLNode *tail = head;
+
+    for (int i = 0; i < len; i++) {
+        tail = appendIntNode(head, arr[i]);
+        tail = tail->next;
+    }
+
+    return head;
+}
