5b9db3d3cf226268540365b55ce5f3af81eed766
Gowtham Behara <gothsiddu@gmail.com>
2018-07-22 00:30:30 +0530

    Test the SLL Util functions

---
+++ Helper/LinkedList/Test/CreateSLL.c
@@ -0,0 +1,17 @@
+#include "../Util/createSLL.h"
+#include "../Util/appendSLLNode.h"
+#include <stdio.h>
+#include "../Util/printList.h"
+
+int main () {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int len = sizeof(arr) / sizeof(arr[0]);
+
+    struct SLLNode* list = createIntSLL(arr, len);
+    printIntList(list);
+
+    appendIntNode(&list, 10);
+    printIntList(list);
+
+    return 0;
+}
