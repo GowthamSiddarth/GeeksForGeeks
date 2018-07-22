54f113c86e0395e36dd32023f600929803013e87
Gowtham Behara <gothsiddu@gmail.com>
2018-07-22 11:41:09 +0530

    Test getNodeAt func

--- Helper/LinkedList/Test/TestIntSLL.c
+++ Helper/LinkedList/Test/TestIntSLL.c
@@ -2,6 +2,7 @@
 #include "../Util/appendSLLNode.h"
 #include "../Util/prependSLLNode.h"
 #include "../Util/sllLength.h"
+#include "../Util/nodeAt.h"
 #include <stdio.h>
 #include "../Util/printList.h"
 
@@ -24,5 +25,19 @@ int main () {
     listLen = lengthOfIntSLL(list);
     printIntList(list);
     printf("listLen = %d\n", listLen);
+
+    int pos[] = {-99, -1, 0, 12, 3, 5, 9, 11, 123, 92983, 10};
+    int numOfPos = sizeof(pos) / sizeof(pos[0]);
+    for (int i = 0; i< numOfPos; i++) {
+        int currPos = pos[i];
+        struct SLLIntNode* currNode = sllIntNodeAt(list, currPos);        
+
+        if (NULL == currNode) {
+            printf("currPos = %d, currNodeVal = %s\n", currPos, "NULL");
+        } else {
+            printf("currPos = %d, currNodeVal = %d\n", currPos, currNode->value);
+        }        
+    }
+
     return 0;
 }
