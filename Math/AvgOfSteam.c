bdf40fcda34c3abe88d9edc43ca6922fceea5a71
Gowtham Behara <gothsiddu@gmail.com>
2018-07-23 09:57:20 +0530

    Use static vars for sum and count

--- Math/AvgOfSteam.c
+++ Math/AvgOfSteam.c
@@ -6,6 +6,13 @@ double getStreamAvgWithPrevAvg(int num, double prevAvg, int count) {
     return (prevAvg * count + num) / (count + 1);
 }
 
+double getStreamAvgWithStaticVars(int num) {
+    static int sum, count;
+
+    sum = sum + num;
+    return ((double) sum) / (++count);
+}
+
 int main () {
     char input[11] = {'\0'};
     int count = 0;
@@ -14,10 +21,11 @@ int main () {
     scanf("%s", input);
     while (0 != cmpStr(input, "n")) {
         int num = atoi(input);
-        double currAvg = getStreamAvgWithPrevAvg(num, prevAvg, count);
-        printf("currAvg = %lf\n", currAvg);
+        double currAvg1 = getStreamAvgWithPrevAvg(num, prevAvg, count);
+        double currAvg2 = getStreamAvgWithStaticVars(num);
+        printf("currAvg1 = %lf, currAvg2 = %lf\n", currAvg1, currAvg2);
 
-        prevAvg = currAvg;
+        prevAvg = currAvg1;
         count++;
         scanf("%s", input);        
     }
