59c64bad81e806c7e2ed65bd4d8db3a884f18556
Gowtham Behara <gothsiddu@gmail.com>
2018-07-23 09:32:40 +0530

    Remove NULL str cmp

--- Helper/String/strcmp.h
+++ Helper/String/strcmp.h
@@ -1,12 +1,4 @@
 int cmpStr(char *s1, char *s2) {
-    if (NULL == s1 && NULL == s2) {
-        return -1;
-    } else if (NULL == s1) {
-        return -s2[0];
-    } else if (NULL == s2) {
-        return s1[0];
-    }
-
     int i = 0;
     while ('\0' != s1[i] && '\0' != s2[i] && s1[i] == s2[i]) {
         i++;
