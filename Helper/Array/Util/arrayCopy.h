1879df97c9a7997272f43956050031bcd17cd407
Gowtham Behara <gothsiddu@gmail.com>
2018-07-28 13:09:09 +0530

    Helper/Array/Util/arrayCopy.h

--- Helper/Array/Util/arrayCopy.h
+++ Helper/Array/Util/arrayCopy.h
@@ -1,3 +1,6 @@
+#ifndef ARRAY_COPY
+#define ARRAY_COPY
+
 void copyIntArr(int *src, int srcLen, int *dest, int destLen, int numOfUnits, int srcStart, int destStart) {
     if (srcStart + numOfUnits <= srcLen && destStart + numOfUnits <= destLen) {
         for (int i = srcStart, j = destStart; i < numOfUnits; i++, j++) {
@@ -5,3 +8,5 @@ void copyIntArr(int *src, int srcLen, int *dest, int destLen, int numOfUnits, in
         }
     }
 }
+
+#endif
