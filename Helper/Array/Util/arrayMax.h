1d110ad86506332c400b48228d35c22c0194cb43
Gowtham Behara <gothsiddu@gmail.com>
2018-08-04 06:20:21 +0530

    Find max in arr using recursion

--- Helper/Array/Util/arrayMax.h
+++ Helper/Array/Util/arrayMax.h
@@ -1,3 +1,5 @@
+#include <limits.h>
+
 int maxIntArr(int *arr, int len) {
     if (len > 0) {
         int maxVal = arr[0];
@@ -14,4 +16,21 @@ int maxIntArr(int *arr, int len) {
     }
 
     return -1;
+}
+
+int maxIntRecHelper(int *arr, int len, int maxVal, int index, int maxIdx) {
+    if (index == len) {
+        return maxVal;
+    } else {
+        if (arr[index] < maxVal) {
+            maxVal = arr[index];
+            maxIdx = index;
+        }
+
+        return maxIntRecHelper(arr, len, maxVal, index + 1, maxIdx);
+    }
+}
+
+int maxIntRec(int *arr, int len) {
+    return maxIntRecHelper(arr, len, INT_MIN, 0, -1);
 }
